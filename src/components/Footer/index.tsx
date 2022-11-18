c4fae7a4cf745300011a857296e98300e54589cb
alexandr2075 <gagara5620@gmail.com>
2022-11-18 10:49:06 +0600

    feat: add main

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,23 +3,23 @@ import './Footer.scss';
 import rs from '../../assets/rs.svg';
 import github from '../../assets/github.png';
 
-const Footer:FC = () => {
+const Footer: FC = () => {
   return (
-	<footer className='footer'>
-        <a className='rs' href='https://rs.school/react/'>
+    <footer className='footer'>
+      <a className='rs' href='https://rs.school/react/'>
         <img src={rs} alt='rs-school' />
-        </a>
-        <a className='github' href='https://rs.school/react/'>
+      </a>
+      <a className='github' href='https://github.com/ZmitserFurmanau'>
         <img src={github} alt='rs-school' />Дмитрий
-        </a>
-        <a className='github' href='https://rs.school/react/'>
+      </a>
+      <a className='github' href='https://github.com/Glav-Tz'>
         <img src={github} alt='rs-school' />Антон
-        </a>
-        <a className='github' href='https://rs.school/react/'>
+      </a>
+      <a className='github' href='https://github.com/alexandr2075'>
         <img src={github} alt='rs-school' />Александр
-        </a>
-        <p>2022</p>
-      </footer>
+      </a>
+      <p>2022</p>
+    </footer>
   )
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,9 +7,9 @@ const Header: FC = () => {
 	<header className='header'>
         <a className='logo' href='/'><img src={logo} alt='logo' /></a>
         <div className='block-sign'>
-          <a className='sign' href='/'>Sign in</a>
-          <a className='sign' href='/'>Sign up</a>
-          <select className='sign sel'>
+          <a className='sign' href='/'><span>Sign in</span></a>
+          <a className='sign' href='/'><span>Sign up</span></a>
+          <select className='select'>
             <option value="value1">EN</option>
             <option value="value2">RU</option>
           </select>
