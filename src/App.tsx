564ce729158b064fef3028072ad1e5d2df34c21d
ZmitserFurmanau <zmitserfurmanau@gmail.com>
2022-11-21 12:06:16 +0200

    feat: add dependencies, pages, router, store, style, hooks, header, assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react';
+import { Outlet } from 'react-router-dom';
 
-function App() {
+import Header from '~/components/Header';
+
+import './style/style.scss';
+
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Outlet />
     </div>
   );
-}
+};
 
 export default App;
