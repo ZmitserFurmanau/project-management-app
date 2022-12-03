ecfc35ce282f646904bf4a535ebf965629b8b2da
ZmitserFurmanau <zmitserfurmanau@gmail.com>
2022-12-03 19:17:09 +0200

    feat: add localization

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,34 @@
 import React, { FC } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
 import { useAppSelector } from '~/hooks/redux';
+import LangCheckbox from '../LangCheckbox';
 
 import styles from './Header.module.scss';
 
 const Header: FC = () => {
   const { isLogged } = useAppSelector(state => state.auth);
+  const { t } = useTranslation();
   return (
     <div className={styles.wrapper}>
       <div className={styles.navbar}>
         {!isLogged && (
           <>
             <NavLink to="/login" className={styles.link}>
-              Войти
+              {t('LOGIN_LINK')}
             </NavLink>
             <NavLink to="/signup" className={styles.link}>
-              Зарегистрироваться
+              {t('SIGNUP_LINK')}
             </NavLink>
           </>
         )}
         {isLogged && (
           <NavLink to="/logout" className={styles.link}>
-            Выйти
+            {t('LOGOUT_LINK')}
           </NavLink>
         )}
+        <LangCheckbox />
       </div>
     </div>
   );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import authReducer from './reducers/authSlice';
+import langReducer from './reducers/langSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    locale: langReducer,
   },
   devTools: process.env.NODE_ENV === 'development',
 });
